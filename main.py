95eeb9989286c989d013d859a14090f0a1adb0c5
AleksBest <bokarev.aleks@mail.ru>
2024-12-21 20:09:19 +0300

    Собрали файл для обучения (20000)

--- main.py
+++ main.py
@@ -14,9 +14,9 @@ data = pd.merge(data, parse, on="ИНН", how="left")
 data["Rating"].fillna(0.2, inplace=True)
 data["RecommendedDealLimit"].fillna(80000, inplace=True)
 
-data["Доходы (тыс, руб.)"] = data.apply(lambda row: row["Revenue"] if pd.isna(row["Доходы (тыс, руб.)"]) else row["Доходы (тыс, руб.)"], axis=1)
-data["Основной ОКВЭД"] = data.apply(lambda row: row["ActivityCode"] if pd.isna(row["Основной ОКВЭД"]) else row["Основной ОКВЭД"], axis=1)
-data["Кол-во сотрудников"] = data.apply(lambda row: row["EmemployeeCount"] if pd.isna(row["Кол-во сотрудников"]) else row["Кол-во сотрудников"], axis=1)
+data["Доходы (тыс, руб.)"] = data.apply(lambda r: r["Revenue"] if pd.isna(r["Доходы (тыс, руб.)"]) else r["Доходы (тыс, руб.)"], axis=1)
+data["Основной ОКВЭД"] = data.apply(lambda r: r["ActivityCode"] if pd.isna(r["Основной ОКВЭД"]) else r["Основной ОКВЭД"], axis=1)
+data["Кол-во сотрудников"] = data.apply(lambda r: r["EmemployeeCount"] if pd.isna(r["Кол-во сотрудников"]) else r["Кол-во сотрудников"], axis=1)
 
 del_props = ["ИНН", "Дата регистрации", "Уставной капитал (руб)", "Адрес", "ФИО Генерального директора", "Дата рождения Генерального директора",
              "ФИО Бенефициара", "Сайт", "Номер телефона", "Провайдер", "Деятельность клиента", "Деятельность клиента со слов клиента",
@@ -39,6 +39,7 @@ labels = [0.5, 0, 0.5, 1]
 data["Кол-во дополнительных ОКВЭДОВ"] = pd.cut(data["Кол-во дополнительных ОКВЭДОВ"], bins=bins, labels=labels, right=False, ordered=False)
 
 data["Система налогообложения "] = data["Система налогообложения "].apply(lambda x: 1 if x != 0 else 0)
+data["Вся негативная информация"] = data["Вся негативная информация"].apply(lambda x: 1 if x != 0 else 0)
 
 data["Отношение оборотов"] = np.where((data["Планируемый оборот по анкете (руб)"] == 0) & (data["Планируемый оборот по снятию д/с (руб)"] == 0), 1,
                                       np.where((data["Планируемый оборот по анкете (руб)"] == 0) & (data["Планируемый оборот по снятию д/с (руб)"] != 0), 1,
@@ -49,10 +50,12 @@ data = data.drop(columns=["Планируемый оборот по анкете
 data.rename(columns={"Rating": "Оценка надежности"}, inplace=True)
 data.rename(columns={"RecommendedDealLimit": "Возможная сумма при 3%"}, inplace=True)
 
+min_max = pd.DataFrame()
 norm_props = ["Доходы (тыс, руб.)", "Налоговая нагрузка", "Кол-во сотрудников", "Возможная сумма при 3%"]
 for prop in norm_props:
     data[prop] = np.log1p(data[prop])
     data["Нормализованно " + prop] = (data[prop] - data[prop].min()) / (data[prop].max() - data[prop].min())
+    min_max["Нормализованно " + prop] = [data[prop].min(), data[prop].max()]
 data = data.drop(columns=norm_props)
 
 data = data.iloc[:, [0, 1, 11, 2, 8, 9, 3, 4, 5, 7, 10, 6, 12]]
@@ -76,8 +79,8 @@ for index, row in data.iterrows():
 
 data["result"] = data["score"].apply(lambda x: 0 if x > (data["score"].min() + data["score"].max()) / 2 else 1)
 
-weights_categories.to_csv('weights.csv', index=False)
 data.to_csv('output.csv', index=False)
+min_max.to_csv('min_max.csv', index=False)
 
 print(data["score"].min())
 print(data["score"].max())
